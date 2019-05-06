396db0f74e2f6a2ffc62e9174c05a8da87fb9a7c
Sean Mussenden <smussenden@gmail.com>
2019-05-06 10:13:39 -0400

    update

--- twitter_keys/httr-keys.r
+++
@@ -1,4 +0,0 @@
-myapp <- oauth_app("smussenden_twitter_app",
-                   key = "ckIXDdBqR7IPkLy35jUkwUM7W",
-                   secret = "51uCttObRrpjfEsnoCVRx4VBm1T07yotOeVZO3rmqc3uCbmpRH"
-)
